298507f926dca91887f34d0fdea5e9eac4a9dcb0
Daniel Riccio <ricciodaniel98@gmail.com>
2024-01-29 18:11:10 -0500

    add command to disable dnd

---
+++ src/commands/link/allowLinks.ts
@@ -0,0 +1,26 @@
+import type { CommandInteraction } from 'discord.js';
+import type { Command } from '../../types/Command';
+import { updateDndEntry } from '../../data/repositories/dndEntryRepository';
+
+export const allowLinks: Command = {
+  data: {
+    name: 'allow-links',
+    description:
+      'Allows you to recieve links once again if you are in the do not disturb list',
+  },
+  execute: async function (interaction: CommandInteraction) {
+    const guild = interaction.guild;
+    const user = interaction.user;
+
+    if (!user) {
+      return;
+    }
+
+    updateDndEntry(user.id, false);
+
+    await interaction.reply({
+      content: 'You are no longer in the do not disturb list ✅',
+      ephemeral: guild ? true : false,
+    });
+  },
+};
