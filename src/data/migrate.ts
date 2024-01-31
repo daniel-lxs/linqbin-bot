64e21ce7d9988e42acd6af99324ec043ef90b4bd
Daniel Riccio <ricciodaniel98@gmail.com>
2024-01-31 18:20:31 -0500

    fix db migration script

--- src/data/migrate.ts
+++ src/data/migrate.ts
@@ -1,7 +1,11 @@
 import { migrate } from 'drizzle-orm/bun-sqlite/migrator';
 import { drizzle } from 'drizzle-orm/bun-sqlite';
 import { Database } from 'bun:sqlite';
+import fs from 'node:fs';
 
+if (!fs.existsSync('./db')) {
+  fs.mkdirSync('./db');
+}
 const sqlite = new Database('./db/sqlite.db', { create: true });
 const db = drizzle(sqlite);
 await migrate(db, { migrationsFolder: './drizzle' });
