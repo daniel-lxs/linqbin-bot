ca5074a7185ee50bb68f1031a9ecda041845b553
Daniel Riccio <ricciodaniel98@gmail.com>
2024-03-07 10:00:20 -0500

    fix: :bug: catch exceptions when executing command functions

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -6,21 +6,25 @@ const interactionCreateListener = {
   name: Events.InteractionCreate,
   async execute(interaction: Interaction) {
     console.log(`Received interaction: ${interaction.id}`);
-    if (interaction.isCommand()) {
-      const { commandName } = interaction;
-      const command = commands[commandName as keyof typeof commands];
-      if (command) {
-        console.log(`Running command: ${commandName}`);
-        command.execute(interaction, command.data);
+    try {
+      if (interaction.isCommand()) {
+        const { commandName } = interaction;
+        const command = commands[commandName as keyof typeof commands];
+        if (command) {
+          console.log(`Running command: ${commandName}`);
+          await command.execute(interaction, command.data);
+        }
       }
-    }
-    if (interaction.isButton()) {
-      const { customId } = interaction;
-      const button = buttons[customId as keyof typeof buttons];
-      if (button) {
-        console.log(`Running button: ${customId}`);
-        button.execute(interaction);
+      if (interaction.isButton()) {
+        const { customId } = interaction;
+        const button = buttons[customId as keyof typeof buttons];
+        if (button) {
+          console.log(`Running button: ${customId}`);
+          await button.execute(interaction);
+        }
       }
+    } catch (error) {
+      console.error(`Error handling interaction: ${error}`);
     }
   },
 };
